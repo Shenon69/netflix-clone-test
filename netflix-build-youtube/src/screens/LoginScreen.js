a9ee3d0c3b7b26300e060980ccaaa6d8ee0f573c
Shenon69 <ask.tikki69@gmail.com>
2023-02-05 00:56:38 +0530

    feat: login screen completed

--- netflix-build-youtube/src/screens/LoginScreen.js
+++ netflix-build-youtube/src/screens/LoginScreen.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./LoginScreen.css";
+import { useState } from "react";
+import SignupScreen from "./SignupScreen";
+
 
 function LoginScreen() {
+
+    const [signIn,setSignIn] = useState(false);
+
   return (
     <div className="LoginScreen">
       <div className="loginscreen_background">
@@ -11,7 +17,27 @@ function LoginScreen() {
           alt=""
         />
 
-        <button className="loginScreen_button">Sign In</button>
+        <button className="loginScreen_button" onClick={() => setSignIn(true)}>Sign In</button>
+        <div className="loginScreen_gradient" />
+      </div>
+
+      <div className="loginScreen_body">
+        {signIn ? (
+            <SignupScreen />
+        ) : (
+        <>
+        <h1>Unlimited films, TV programmes and more.</h1>
+        <h2>Watch anywhere. Cancel at any time.</h2>
+        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+        <div className="loginScreen_input">
+            <form>
+                <input type="email" placeholder="Email address" />
+                <button className="loginScreen_getStarted" onClick={() => setSignIn(true)}>GET STARTED</button>
+            </form>
+        </div>
+        </>
+
+        )}
       </div>
     </div>
   );
