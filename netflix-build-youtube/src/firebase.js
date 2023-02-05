fb3cd334b903c9a527def00489db0df349463b1b
Shenon69 <ask.tikki69@gmail.com>
2023-02-06 00:56:08 +0530

    dev: Sign Up screen css and user table using firebase

--- netflix-build-youtube/src/firebase.js
+++ netflix-build-youtube/src/firebase.js
@@ -1,3 +1,9 @@
+import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyBRMa2zoL_FtYWRzC4JZ_iCFYYyduNvX_U",
     authDomain: "netflix-clone-7fce4.firebaseapp.com",
@@ -6,3 +12,11 @@ const firebaseConfig = {
     messagingSenderId: "55746926003",
     appId: "1:55746926003:web:279223cebdec7e2d10e7c6"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const db = firebaseApp.firestore();
+  
+
+  export {auth};
+  export default db;
